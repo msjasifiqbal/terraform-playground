ed20f3ff3982026058fef5a55d7c01da36306b9b
msjasifiqbal <msjasifiqbal@hotmail.com>
2024-05-04 08:07:36 +0530

    changesinmaintf

--- lab1/main.tf
+++ lab1/main.tf
@@ -1,1 +1,20 @@
-test
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rgcreate" {
+  name     = "AsifRG-TF"
+  location = "East US"
+}
