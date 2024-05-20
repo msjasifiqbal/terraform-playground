eb99a3eb00cd10cbb1f9bf1c17cd197001d4e043
msjasifiqbal <msjasifiqbal@hotmail.com>
2024-05-20 21:41:44 +0530

    devlab2commit

--- devlab2/main.tf
+++ devlab2/main.tf
@@ -13,87 +13,86 @@ provider "azurerm" {
 }
 
 data "azurerm_resource_group" "rg" {
-  name = "AsifRG-TF"
+  name = "AsifRG"
 }
 
-
+//declaring vaiables for vnet and subnet
 variable "vnet_details" {
-  description = "Details of the VNETs and their subnets"
+  description = "Details for the virtual networks to be created"
   type = map(object({
     address_space = list(string)
-    subnets = list(object({
-      name    = string
-      newbits = number
-      netnum  = number
-    }))
+    subnets       = list(string)
   }))
 
   default = {
     vnet1 = {
-      address_space = ["10.1.0.0/16"]
-      subnets = [
-        { name = "Web", newbits = 8, netnum = 1 },
-        { name = "App", newbits = 8, netnum = 2 }
-      ]
-    }
+      address_space = ["10.0.0.0/16"]
+      subnets       = ["subnet1", "subnet2"]
+    },
     vnet2 = {
-      address_space = ["10.2.0.0/16"]
-      subnets = [
-        { name = "Web", newbits = 8, netnum = 1 },
-        { name = "DB", newbits = 8, netnum = 2 }
-      ]
+      address_space = ["10.1.0.0/16"]
+      subnets       = ["subnet1", "subnet2"]
     }
   }
 }
 
-
-resource "azurerm_virtual_network" "vnet" {
-  for_each = var.vnet_details
-
-  name                = each.key
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
-  address_space       = each.value.address_space
-}
-
+//used local variable with flatten to generate the values
 locals {
-  flat_subnets = flatten([
-    for vnet, details in var.vnet_details : [
-      for subnet in details.subnets : {
-        vnet_name   = vnet
-        subnet_name = subnet.name
-        newbits     = subnet.newbits
-        netnum      = subnet.netnum
+  subnet_details = flatten([
+    for vnet_key, vnet_value in var.vnet_details : [
+      for subnet in vnet_value.subnets : {
+        vnet_key = vnet_key
+        subnet_name   = subnet
       }
     ]
   ])
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  for_each = var.vnet_details
+
+  name                = each.key
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  address_space       = each.value.address_space
+}
+
 resource "azurerm_subnet" "subnet" {
-  for_each = { for subnet in local.flat_subnets : "${subnet.vnet_name}-${subnet.subnet_name}" => subnet }
+  for_each = { for subnet in local.subnet_details : "${subnet.vnet_key}-${subnet.subnet_name}" => subnet }
 
   name                 = each.value.subnet_name
   resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet[each.value.vnet_name].name
-  address_prefixes     = [cidrsubnet(azurerm_virtual_network.vnet[each.value.vnet_name].address_space[0], each.value.newbits, each.value.netnum)]
-
-  depends_on = [
-    azurerm_virtual_network.vnet
-  ]
+  virtual_network_name = azurerm_virtual_network.vnet[each.value.vnet_key].name
+  address_prefixes     = [cidrsubnet(azurerm_virtual_network.vnet[each.value.vnet_key].address_space[0], 8, index(var.vnet_details[each.value.vnet_key].subnets, each.value.subnet_name))]
 }
 
+
+resource "azurerm_network_security_group" "nsg" {
+  for_each = { for subnet in local.subnet_details : "${subnet.vnet_key}-${subnet.subnet_name}" => subnet }
+
+  name                = "${each.value.subnet_name}-nsg"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  for_each = { for subnet in local.subnet_details : "${subnet.vnet_key}-${subnet.subnet_name}" => subnet }
+
+  subnet_id                 = azurerm_subnet.subnet[each.key].id
+  network_security_group_id = azurerm_network_security_group.nsg[each.key].id
+}
 
 
 variable "rules_file" {
   description = "The path to the CSV file containing the rules"
   type        = string
-  default     = "rules.csv"
+  default     = "nsgrules.csv"
 }
 
 locals {
   rules_csv = csvdecode(file(var.rules_file))
-  nsg_name = { for subnet in local.flat_subnets : "${subnet.vnet_name}-${subnet.subnet_name}" => {
-    name = "${subnet.vnet_name}-${subnet.subnet_name}"
+  nsg_name = { for subnet in local.subnet_details : "${subnet.vnet_key}-${subnet.subnet_name}" => {
+    name = "${subnet.vnet_key}-${subnet.subnet_name}"
   } }
 }
 
@@ -118,16 +117,4 @@ resource "azurerm_network_security_group" "this" {
       destination_address_prefix = security_rule.value.destination_address_prefix
     }
   }
-}
-
-resource "azurerm_subnet_network_security_group_association" "this" {
-  for_each = local.nsg_name
-
-  subnet_id                 = azurerm_subnet.subnet[each.key].id
-  network_security_group_id = azurerm_network_security_group.this[each.key].id
-
-  depends_on = [
-    azurerm_subnet.subnet,
-    azurerm_network_security_group.this
-  ]
 }
